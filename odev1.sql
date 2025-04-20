f109626ce9692d0046958f6a23870b20163b52d5
erenabi38 <erenyavuz353@icloud.com>
2025-04-20 22:21:28 +0200

    Add files via upload

--- odev1.sql
+++ odev1.sql
@@ -10,7 +10,7 @@ ve aynı zamanda rating 'G' ye eşittir?
 country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
 Kolay Gelsin.
-*/
+
 SELECT DISTINCT replacement_cost FROM film;
 SELECT COUNT(DISTINCT replacement_cost) FROM film;
 SELECT title FROM film
@@ -19,3 +19,29 @@ SELECT * FROM country
 WHERE country LIKE '_____';
 SELECT COUNT(*) FROM city
  WHERE city ILIKE '%r';
+
+
+SELECT * FROM film
+WHERE replacement_cost = 14.99 AND rental_rate = 0.99
+ORDER BY length DESC
+LIMIT 5;
+
+SELECT * FROM actor
+WHERE first_name = 'Penelope'
+ORDER BY last_name
+OFFSET 2
+LIMIT 1;*/
+/*film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+Kolay Gelsin.
+SELECT title,length FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+*/
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
